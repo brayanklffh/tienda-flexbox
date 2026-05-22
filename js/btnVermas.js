0250fafa16f9dd2d0164f58f4407be4a785316b2
brayanklffh <159786545+brayanklffh@users.noreply.github.com>
2026-05-21 22:17:00 -0500

    modificaciones aspecto tienda

--- js/btnVermas.js
+++ js/btnVermas.js
@@ -2,13 +2,16 @@
 
 const prendasOcultas = document.querySelectorAll(".prendas__ocultas");
 const btnVerMas = document.querySelector(".ver__mas");
+const cardSuelta = document.querySelector(".cuartoproducto");
 
 btnVerMas.addEventListener("click", () => {
   prendasOcultas.forEach((prenda) => {
     if (prenda.style.display === "block") {
       prenda.style.display = "none";
+      cardSuelta.style.display="none";
     } else {
       prenda.style.display = "block";
+      cardSuelta.style.display="block";
     }
   });
 
@@ -17,4 +20,7 @@ btnVerMas.addEventListener("click", () => {
   } else {
     btnVerMas.textContent = "Ver menos";
   }
+  //865 hasta 1366 hacer config en css
+  //solucionar error imagen 1030
+  //usar matchmedia para la ultima configuracion
 });
