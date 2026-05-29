ae22a8ac8c5278cb9b83b2ab1d878d9b01c841bd
brayanklffh <159786545+brayanklffh@users.noreply.github.com>
2026-05-28 20:16:33 -0500

    mejoras

--- js/btnVermas.js
+++ js/btnVermas.js
@@ -3,16 +3,20 @@
 const prendasOcultas = document.querySelectorAll(".prendas__ocultas");
 const btnVerMas = document.querySelector(".ver__mas");
 const cardSuelta = document.querySelector(".cuartoproducto");
-
+const productos = document.querySelector(".oculta");
 btnVerMas.addEventListener("click", () => {
+   
   prendasOcultas.forEach((prenda) => {
+   
     if (prenda.style.display === "block") {
       prenda.style.display = "none";
-      cardSuelta.style.display="none";
+     
     } else {
       prenda.style.display = "block";
-      cardSuelta.style.display="block";
+   
+      
     }
+
   });
 
   if (btnVerMas.textContent === "Ver menos") {
@@ -20,7 +24,17 @@ btnVerMas.addEventListener("click", () => {
   } else {
     btnVerMas.textContent = "Ver menos";
   }
+
+      if (
+      window.matchMedia("(min-width: 865px) and (max-width: 1367px)").matches) {
+      cardSuelta.classList.toggle("prendas__ocultas");
+
+    } 
+    
+
+
   //865 hasta 1366 hacer config en css
   //solucionar error imagen 1030
-  //usar matchmedia para la ultima configuracion
+  //usar matchmedia en pixel 966 a 865px
 });
+
